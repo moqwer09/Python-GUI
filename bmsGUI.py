cfdb8f25e110ae652bb9c1ae57942eeaeea74487
moqwer09 <moqwer09@gmail.com>
2024-10-28 21:33:01 -0400

    started something

--- bmsGUI.py
+++ bmsGUI.py
@@ -1,6 +1,28 @@
-import serial.tools.list_ports
+import serial.tools.list_ports # type: ignore
 from tkinter import *
 from tkinter import ttk
 
+#Initialize serial and tkinter
 ports = serial.tools.list_ports.comports()
 serialInst = serial.Serial()
+root = Tk(screenName="BMS GUI", baseName="Main Screen")
+root.title("BMS GUI")
+root.geometry("640x480")
+scrollbar = Scrollbar(root)
+scrollbar.pack(side = RIGHT, fill=Y)
+
+#Get all com ports
+portList = Listbox(root, yscrollcommand=Scrollbar.set)
+
+for port in ports:
+    portList.insert(END, str(port))
+    print(str(port))
+
+portList.pack(side=LEFT, fill=BOTH)
+scrollbar.config(command=portList.yview)
+
+
+
+
+#End of tkinter
+root.mainloop()
