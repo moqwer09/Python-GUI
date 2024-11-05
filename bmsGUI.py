bf7e9f2a20dc30fa99cf5fc8333b5aae5bf5adac
moqwer09 <moqwer09@gmail.com>
2024-11-05 16:35:35 -0500

    kinda works

--- bmsGUI.py
+++ bmsGUI.py
@@ -1,94 +1,177 @@
 import serial.tools.list_ports 
 import tkinter as tk
 import ttkbootstrap as ttk
+from time import sleep
 
 #Initialize serial
-ports = serial.tools.list_ports.comports()
 serialInst = serial.Serial()
 serialInst.baudrate = 9600
 
-def updatePorts():
-    ports = serial.tools.list_ports.comports()
-    print('Com ports updated') #Testing
-
 #Tkinter Window Loop
-root = ttk.Window(scaling = 2.0, themename='darkly')
+root = ttk.Window(themename='darkly')
 root.title("BMS GUI")
-root.minsize(1280,600)
-root.columnconfigure(0, weight=1)
-root.columnconfigure(1, weight=1)
-root.rowconfigure(0, weight=1)
+root.minsize(1280,720)
 
-#Left side lowest/highest voltages/temps also com port selection
+voltages = [[tk.StringVar(value='0V') for tabs in range(14)] for boards in range(10)]
+temps = [[tk.StringVar(value='0C') for tabs in range(9)] for boards in range(10)]
+
+def updateVals():
+    if serialInst.is_open:
+        serialInst.reset_input_buffer()
+        packets = []
+        while not(bool(serialInst.in_waiting)):
+            #print('nothing')
+            sleep(0.1)
+        
+        sleep(0.1)
+        #print('Updating Vals')
+        while serialInst.in_waiting:
+            packets.append(serialInst.readline().decode('utf').rstrip('\n\r'))
+        
+        if len(packets) > 293:
+            del packets[171:261]
+        
+        for board in range(10):
+            #print('board' + str(board))
+            for tap in range(14):
+                #maxVolt = max(maxVolt, float(packets[2 + tap + 17*board]))
+                voltages[board][tap].set(str(packets[2 + tap + 17*board]) + 'V')
+        
+        for board in range(10):
+            for temp in range(9):
+                #maxTemp = max(maxTemp, 0.0 if float(packets[173 + temp + 12*board]) == 150.0 else float(packets[173 + temp + 12*board]))
+                temps[board][temp].set(str(packets[173 + temp + 12*board]) + 'C')
+
+
+    root.after(1000, updateVals)
+
+def updateClear():
+    if serialInst.is_open:
+        updateVals()
+
+root.after(1000, updateVals)
+
+#Left side lowest/highest voltages/temps also comport selection
 leftFrame = ttk.Frame(master=root)
-leftFrame.columnconfigure(0, weight=1)
-leftFrame.rowconfigure(0, weight=1)
-leftFrame.rowconfigure(1, weight=1)
 
 minMaxFrame = ttk.Frame(master=leftFrame)
 comportFrame = ttk.Frame(master=leftFrame)
 
-#Min Max Frame Setup
-minMaxFrame.columnconfigure(0, weight=1)
-minMaxFrame.columnconfigure(1, weight=1)
-minMaxFrame.rowconfigure(0, weight=1)
-minMaxFrame.rowconfigure(1, weight=1)
-minMaxFrame.rowconfigure(2, weight=1)
-minMaxFrame.rowconfigure(3, weight=1)
-minMaxFrame.rowconfigure(4, weight=1)
-minMaxFrame.rowconfigure(5, weight=1)
-minMaxFrame.rowconfigure(6, weight=1)
-
 #Min Max
-resetGlobalMax = ttk.Button(master=minMaxFrame, text='Reset All Time Max')
-resetGlobalMax.grid(row=0, column=0, columnspan=2, sticky='nsew')
+resetGlobalMax = ttk.Button(master=minMaxFrame, text='Reset All Time Values')
+resetGlobalMax.pack(side='top', fill='x')
 
 #Min Vals
-minLabel = ttk.Label(master=minMaxFrame, text='Min Values', anchor="center")
-minLabel.grid(row=1, column=0, columnspan=2, sticky='nsew')
-lowVoltLabel = ttk.Label(master=minMaxFrame, text='Voltage:', anchor="center")
-lowVoltLabel.grid(row=2, column=0, sticky='new')
-lowVoltVal = ttk.Label(master=minMaxFrame, text='0V', anchor="center")
-lowVoltVal.grid(row=2, column=0, sticky='sew')
-lowTempLabel = ttk.Label(master=minMaxFrame, text='Temp:', anchor="center")
-lowTempLabel.grid(row=2, column=1, sticky='new')
-lowTempVal = ttk.Label(master=minMaxFrame, text='0C', anchor="center")
-lowTempVal.grid(row=2, column=1, sticky='sew')
+minLabel = ttk.Label(master=minMaxFrame, text='Min Values', anchor="center", relief='solid', borderwidth=1)
+minLabel.pack(side='top', fill='both')
+minFrame = ttk.Frame(master=minMaxFrame, relief='solid', borderwidth=1)
+#volt
+minVoltFrame = ttk.Frame(master=minFrame, relief='solid', borderwidth=1)
+minVoltLabel = ttk.Label(master=minVoltFrame, text='Voltage:', anchor="center")
+minVoltLabel.pack(side='top')
+minVoltVal = ttk.Label(master=minVoltFrame, text='0V', anchor="center")
+minVoltVal.pack(side='top')
+minVoltFrame.pack(side='left', expand=True, fill='both')
+#temp
+minTempFrame = ttk.Frame(master=minFrame, relief='solid', borderwidth=1)
+minTempLabel = ttk.Label(master=minTempFrame, text='Temp:', anchor="center")
+minTempLabel.pack(side='top')
+minTempVal = ttk.Label(master=minTempFrame, text='0C', anchor="center")
+minTempVal.pack(side='top')
+minTempFrame.pack(side='left', expand=True, fill='both')
+
+minFrame.pack(side='top', expand=False, fill='x')
 
 #High Vals
-maxLabel = ttk.Label(master=minMaxFrame, text='Max Current Values', anchor="center")
-maxLabel.grid(row=3, column=0, columnspan=2, sticky='nsew')
-maxVoltLabel = ttk.Label(master=minMaxFrame, text='Voltage:', anchor="center")
-maxVoltLabel.grid(row=4, column=0, sticky='new')
-maxVoltVal = ttk.Label(master=minMaxFrame, text='0V', anchor="center")
-maxVoltVal.grid(row=4, column=0, sticky='sew')
-maxTempLabel = ttk.Label(master=minMaxFrame, text='Temp:', anchor="center")
-maxTempLabel.grid(row=4, column=1, sticky='new')
-maxTempVal = ttk.Label(master=minMaxFrame, text='0C', anchor="center")
-maxTempVal.grid(row=4, column=1, sticky='sew')
+maxLabel = ttk.Label(master=minMaxFrame, text='Max Values', anchor="center")
+maxLabel.pack(side='top', fill='x')
+maxFrame = ttk.Frame(master=minMaxFrame)
+#volt
+maxVoltFrame = ttk.Frame(master=maxFrame)
+maxVoltLabel = ttk.Label(master=maxVoltFrame, text='Voltage:', anchor="center")
+maxVoltLabel.pack(side='top')
+maxVoltVal = ttk.Label(master=maxVoltFrame, text='0V', anchor="center")
+maxVoltVal.pack(side='top')
+maxVoltFrame.pack(side='left', expand=True, fill='both')
+#temp
+maxTempFrame = ttk.Frame(master=maxFrame)
+maxTempLabel = ttk.Label(master=maxTempFrame, text='Temp:', anchor="center")
+maxTempLabel.pack(side='top')
+maxTempVal = ttk.Label(master=maxTempFrame, text='0C', anchor="center")
+maxTempVal.pack(side='top')
+maxTempFrame.pack(side='left', expand=True, fill='both')
 
-#High All Time Volt
-maxAllLabel = ttk.Label(master=minMaxFrame, text='Max All-Time Values', anchor="center")
-maxAllLabel.grid(row=5, column=0, columnspan=2, sticky='nsew')
-maxAllVoltLabel = ttk.Label(master=minMaxFrame, text='Voltage:', anchor="center")
-maxAllVoltLabel.grid(row=6, column=0, sticky='new')
-maxAllVoltVal = ttk.Label(master=minMaxFrame, text='0V', anchor="center")
-maxAllVoltVal.grid(row=6, column=0, sticky='sew')
-maxAllTempLabel = ttk.Label(master=minMaxFrame, text='Temp:', anchor="center")
-maxAllTempLabel.grid(row=6, column=1, sticky='new')
-maxAllTempVal = ttk.Label(master=minMaxFrame, text='0C', anchor="center")
-maxAllTempVal.grid(row=6, column=1, sticky='sew')
-
-minMaxFrame.grid(row=0, column=0, sticky='nsew')
-
-#Comport Frame
+maxFrame.pack(side='top', expand=False, fill='x')
 
 
-leftFrame.grid(row=0, column=0, sticky='nsew')
+#All Time High Vals
+maxAllLabel = ttk.Label(master=minMaxFrame, text='All Time Max Values', anchor="center")
+maxAllLabel.pack(side='top', fill='x')
+maxAllFrame = ttk.Frame(master=minMaxFrame)
+#volt
+maxAllVoltFrame = ttk.Frame(master=maxAllFrame)
+maxAllVoltLabel = ttk.Label(master=maxAllVoltFrame, text='Voltage:', anchor="center")
+maxAllVoltLabel.pack(side='top')
+maxAllVoltVal = ttk.Label(master=maxAllVoltFrame, text='0V', anchor="center")
+maxAllVoltVal.pack(side='top')
+maxAllVoltFrame.pack(side='left', expand=True, fill='both')
+#temp
+maxAllTempFrame = ttk.Frame(master=maxAllFrame)
+maxAllTempLabel = ttk.Label(master=maxAllTempFrame, text='Temp:', anchor="center")
+maxAllTempLabel.pack(side='top')
+maxAllTempVal = ttk.Label(master=maxAllTempFrame, text='0C', anchor="center")
+maxAllTempVal.pack(side='top')
+maxAllTempFrame.pack(side='left', expand=True, fill='both')
+
+maxAllFrame.pack(side='top', expand=False, fill='x')
+
+minMaxFrame.pack(side='top', expand=False, fill='both')
+
+#Comport Frame setup
+
+portTable = ttk.Treeview(master=comportFrame, columns=('comName', 'comDesc'), show='headings', selectmode='browse')
+portTable.heading('comName', text='Port')
+portTable.column('comName', width=60)
+portTable.heading('comDesc', text='Description')
+portTable.column('comDesc', width=250)
+
+def updatePorts():
+    portTable.delete(*portTable.get_children())
+    ports = serial.tools.list_ports.comports()
+    for i in range(len(ports)):
+        data = (str(ports[i].name), str(ports[i].description))
+        portTable.insert(parent='', index=i, values=data)
+
+def openPort(_):
+    if serialInst.is_open:
+        serialInst.close()
+    
+    serialInst.port = str(portTable.item(portTable.selection())['values'][0])
+    serialInst.open()
+
+    if serialInst.is_open:
+        print('Port opened')
+    else:
+        print('Port did not open')
+
+portTable.bind('<<TreeviewSelect>>', openPort)
+
+updatePortsButton = ttk.Button(master=comportFrame, text='Update Com Ports', command=updatePorts)
+updatePortsButton.pack(side='top', fill='x')
+
+portTable.pack(side='top', expand=True, fill='both')
+
+comportFrame.pack(side='top', expand=True, fill='both')
+
+leftFrame.pack(side='left', fill='y')
 
 #Add tabs for each board
 notebook = ttk.Notebook(master=root)
 tabs = {}
+    
+
+#tapLabels = [[ttk.Label(master=tabs[boards]) for taps in range(14)] for boards in range(10)]
+
 
 for i in range(10):
     #Tab Setup
@@ -114,8 +197,11 @@ for i in range(10):
     #Placing Tap Labels
     for row in range(2):
         for column in range(7):
-            tapLabel = ttk.Label(master=tapFrame, text=('Tap ' + str(((row*7 + column) + 1))))
-            tapLabel.grid(row=row, column=column, sticky='nsew')
+            #voltages[i][row*7 + column] = str(i*10 + row*7 + column) + 'V' #Testing
+            tapLabel = ttk.Label(master=tapFrame, text=('Tap ' + str(((row*7 + column) + 1)) + ': '))
+            tapVal = ttk.Label(master=tapFrame, textvariable=voltages[i][row*7 + column])
+            tapLabel.grid(row=row, column=column, sticky='new')
+            tapVal.grid(row=row, column=column, sticky='sew')
     #Place Frame
     tapFrame.grid(row=1, column=0, sticky='nsew')
     
@@ -130,34 +216,18 @@ for i in range(10):
         tempFrame.columnconfigure(column, weight=1)
     #Placing Tap Labels
     for column in range(9):
+        #temps[i][column] = str(i*10 + column) + 'C' #Testing
         tempLabel = ttk.Label(master=tempFrame, text=('Temp ' + str(column+1)))
-        tempLabel.grid(row=0, column=column, sticky='nsew')
+        tempVal = ttk.Label(master=tempFrame, textvariable=temps[i][column])
+        tempLabel.grid(row=0, column=column, sticky='new')
+        tempVal.grid(row=0, column=column, sticky='sew')
     #Place Frame
         tempFrame.grid(row=3, column=0, sticky='nsew')
 
     #Add Whole Frame
     notebook.add(tabs[i], text='Board ' + str(i+1))
 
-notebook.grid(row=0, column=1, sticky='nsew')
-
-#Com port selection menu
-menu = tk.Menu(master=root)
-
-#Update com ports
-comMenu = tk.Menu(master=menu, tearoff=False)
-comMenu.add_command(label='Update Com Ports', command=updatePorts)
-menu.add_cascade(label='Com Port Configuration', menu=comMenu)
-
-#Com Ports Sub Menu Selection
-comPortsMenu_sub = tk.Menu(menu, tearoff=False)
-
-for port in range(len(ports)):
-    comPortsMenu_sub.add_checkbutton(label=str(ports[port]))
-
-comMenu.add_cascade(label='Com Ports:', menu=comPortsMenu_sub)
-
-#Add Menu
-root.configure(menu=menu)
+notebook.pack(side='left', expand=True, fill='both')
 
 #End of tkinter loop
 root.mainloop()
